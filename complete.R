803a61dbbf6440da20035be658c9d50bbc154d57
swirfneblin <swirfneblin@gmail.com>
2015-06-24 18:01:01 -0300

    Exercicio concluido

--- complete.R
+++ complete.R
@@ -1,13 +1,11 @@
 complete <- function(directory, id = 1:332) {
    
-    require("stringr")
-    dataNobs <- data.frame(id = numeric(0), nobs = numeric(0))
-    
-    for(i in id){
-        fileName <- paste(str_pad(i, width = 3, side = "left", pad = "0"), "csv", sep = ".")
-        fullPath <- paste(directory, fileName, sep = "/")
-        data <- read.csv(fullPath)
-        dataNobs <- rbind(dataNobs, data.frame(id = i, nobs = nrow(data)))
+    nobs = numeric()
+    for (i in id) {
+        
+        newRead = read.csv(paste(directory, "/", formatC(i, width = 3, flag = "0"), 
+                                 ".csv", sep = ""))
+        nobs = c(nobs, sum(complete.cases(newRead)))
     }
-    dataNobs
+    return(data.frame(id, nobs))
 }
